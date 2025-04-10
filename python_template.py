ac550903e12d50b6e0faefe373bb54fbc3f8204a
Sarisht shreshth <133949606+sarishtshreshth0@users.noreply.github.com>
2025-04-10 21:50:58 +0530

    python_template.py

---
+++ python_template.py
@@ -0,0 +1,47 @@
+import math
+import heapq
+from heapq import heappop, heappush
+MOD = 10**9 + 7
+
+## power
+def power(x, y, p=MOD):
+    res = 1
+    while y:
+        if y & 1: res = res * x % p
+        x = x * x % p
+        y //= 2
+    return res
+
+## gcd and lcm
+def gcd(a, b): return math.gcd(a, b)
+def lcm(a, b): return (a * b) // gcd(a, b)
+
+## prefix sum
+def prefix_sum(arr):
+    psum = [0] * (len(arr) + 1)
+    for i in range(len(arr)): psum[i+1] = psum[i] + arr[i]
+    return psum
+
+## number of set bits 
+def number_of_set_bits(n):
+    count = 0
+    while n > 0:
+        n = n & (n - 1)
+        count += 1
+    return count
+
+def sieve(n):
+    is_prime = [True] * (n + 1)
+    is_prime[0] = is_prime[1] = False
+    for i in range(2, int(n**0.5) + 1):
+        if is_prime[i]:
+            for j in range(i * i, n + 1, i):
+                is_prime[j] = False
+    return [i for i in range(n + 1) if is_prime[i]]
+
+def number(): return int(input())
+def numbers(): return map(int , input().split())
+def lists(): return list(map(int, input().split()))
+def s(): return input()
+
+##-------------------------------------------------------------------##
